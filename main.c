a5b84c7ff4a6f5e28b40fae7ca762d8cd85134fa
remy <hamedremy@hotmail.fr>
2024-06-25 12:39:12 +0200

    wip

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fun/fun.h"
+#include <string.h>
+#include "arguments/arguments.h"
+
 
 int main(int argc, char **argv) {
 
+    chekcArguments(argc);
+
+    char * operationType = malloc(sizeof(char) * 100);
+    char *filename = malloc(sizeof(char) * 100);
+
+    strcopy(operationType, argv[1]);
+    strcopy(filename, argv[2]);
+
+    int operation = identifyOperationRequired(operationType);
+
+    printf("operation: %d\n", operation);
+
+    if(operation == 1) {
+        printf("addition\n");
+        
+    } else {
+        printf("multiplication\n");
+    }
+
+    free(operationType);
+
+    free(filename);
+
+
+    
     
 
     return 0;
